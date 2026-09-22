745e620742b5650b9a3f7fd6f29c0e2a57048bff
ShashiKumar-S <shashikumarspatil999@gmail.com>
2026-09-22 18:35:26 +0530

    save the file

--- sql01.sql
+++ sql01.sql
@@ -1,7 +1,4 @@
-#Query all columns for all American cities in the CITY table #with populations larger than 100000. The CountryCode for #America is USA.
-
-#The CITY table is described as follows:
-SELECT *
+SELECT * 
 FROM CITY
 WHERE CountryCode = 'USA'
-  AND Population > 100000;
+AND Population > 100000;
